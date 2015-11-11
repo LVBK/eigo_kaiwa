f1f9ccc714b6c3df1c580cad2e1f2367f868d4d6
Nguyễn Tú Nam <kiemmadoccocaubai.vn@gmail.com>
2015-11-11 03:42:26 +0000

    Change title of website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+       base_title = "英語会話" 
+       if page_title.empty?
+           base_title
+       else
+           "#{page_title}"
+       end
+    end
 end
