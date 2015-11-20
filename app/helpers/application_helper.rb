35ef1bc663367f3acd825ee79b4a2ae558e3a8f8
Nguyễn Tú Nam <kiemmadoccocaubai.vn@gmail.com>
2015-11-20 12:47:24 +0000

    change title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
     def full_title(page_title = '')
-       base_title = "英語会話" 
+       base_title = "英語会話 | Eigo Kaiwa" 
        if page_title.empty?
            base_title
        else
-           "#{page_title}"
+           "#{page_title} | #{base_title}"
        end
     end
 end
