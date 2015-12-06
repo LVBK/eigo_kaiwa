a0e95c7cbcebd781e1be414659a178145686b068
Nguyễn Tú Nam <kiemmadoccocaubai.vn@gmail.com>
2015-12-06 13:09:13 +0000

    UI modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,20 @@ module ApplicationHelper
            "#{page_title} | #{base_title}"
        end
     end
+    
+    def resource_name
+	  :user
+	end
+
+	def resource
+	  @resource ||= User.new
+	end
+
+	def devise_mapping
+	  @devise_mapping ||= Devise.mappings[:user]
+	end
+
+	def resource_class
+	  devise_mapping.to
+	end
 end
